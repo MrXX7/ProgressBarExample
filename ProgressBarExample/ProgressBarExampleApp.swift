d4091d4c10404c274bbbca90fa6bacb2b5b2678e
Oncu <oohancan@gmail.com>
2022-10-31 11:08:19 +0300

    Initial Commit

---
+++ ProgressBarExample/ProgressBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarExampleApp.swift
+//  ProgressBarExample
+//
+//  Created by Oncu Can on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
